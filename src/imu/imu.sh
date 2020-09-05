9a85c6df00ae82cfbc206c37fdf08c7448c431de
tb <zuowenchao0628@qq.com>
2020-09-05 15:11:27 +0800

    1

---
+++ src/imu/imu.sh
@@ -0,0 +1,3 @@
+source /home/tb/Ros/catkin_imu/devel/setup.bash
+sudo chmod 777 /dev/ttyUSB*
+roslaunch lpms_imu imu_public.launch
---
+++ src/velodyne/velodyne.sh
@@ -0,0 +1,2 @@
+source /home/tb/Ros/catkin_velodyne/devel/setup.bash
+roslaunch velodyne_pointcloud VLP16_points.launch calibration:=/home/tb/VLP-16.yaml
